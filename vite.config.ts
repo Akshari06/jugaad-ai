b3bfa7b35cdfdba4e7fd6f819f83ff7d57fe6e78
Akshari06 <akshari200618@gmail.com>
2026-05-01 19:33:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/jugaad-ai/',   // 🔥 THIS IS CRITICAL
+  base: '/jugaad-ai/',
   plugins: [react()],
 })
