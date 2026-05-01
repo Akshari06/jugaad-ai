340576429c50ce732a0b8512c3960d893973e006
Akshari06 <akshari200618@gmail.com>
2026-05-01 19:26:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-
-  return {
-    base: '/jugaad-ai/',,  // 👈 THIS is the only important addition
-
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-
-    plugins: [react(), tailwindcss()],
-
-    define: {
-      'process.env.API_KEY': JSON.stringify("AIzaSyDRI0k1vcQSjnNh5xHnyT6MI4ZbAv3muOA"),
-      'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyDRI0k1vcQSjnNh5xHnyT6MI4ZbAv3muOA"),
-    },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
-});
+export default defineConfig({
+  base: '/jugaad-ai/',   // 🔥 THIS IS CRITICAL
+  plugins: [react()],
+})
